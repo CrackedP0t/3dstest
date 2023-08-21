64624a89cfcdb1cd4ca702cbbab67bd8b82fd040
Elaina Martineau <elainamartineau@gmail.com>
2023-08-21 14:39:50 -0600

    Depth mostly working

--- source/main.c
+++ source/main.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include "vshader_shbin.h"
-#include "raysQ_t3x.h"
+#include "raysQ_256_t3x.h"
 #define max(a,b)             \
 ({                           \
     __typeof__ (a) _a = (a); \
@@ -35,18 +35,19 @@ static DVLB_s *vshader_dvlb;
 static shaderProgram_s program;
 static int uLoc_projection;
 static int uLoc_tint;
+static int uLoc_side;
 static C3D_Mtx projection;
 
 static vertex *vbo_data;
 static C3D_Tex texture;
 
 #define MAX_SPRITES (15000)
-#define SPRITE_HEIGHT (25.0f)
-#define SPRITE_WIDTH (25.0f)
-#define ORIGINAL_WIDTH (112.0)
-#define ORIGINAL_HEIGHT (112.0)
+#define SPRITE_HEIGHT (64.0f)
+#define SPRITE_WIDTH (64.0f)
+#define ORIGINAL_WIDTH (256.0)
+#define ORIGINAL_HEIGHT (256.0)
 
-static int current_sprites = 2000;
+static int current_sprites = 10;
 static spriteinfo sprites[MAX_SPRITES];
 
 // Helper function for loading a texture from memory
@@ -65,17 +66,17 @@ float randbetween(float min, float max) {
 	return (float)rand() / RAND_MAX * (max - min) + min;
 }
 
-void add_rect(vertex * dest, float x, float y, float width, float height) {
+void add_rect(vertex * dest, float x, float y, float z, float width, float height) {
 	float u =  (float)ORIGINAL_WIDTH / (float)texture.width;
 	float v =  1.0 - (float)ORIGINAL_HEIGHT / (float)texture.height;
 
 	vertex vertex_list[] = {
-		{{x, y, 1.0}, {0.0, 1.0}},
-		{{x + width, y, 1.0}, {u, 1.0}},
-		{{x, y + height, 1.0}, {0.0, v}},
-		{{x, y + height, 1.0}, {0.0, v}},
-		{{x + width, y, 1.0}, {u, 1.0}},
-		{{x + width, y + height, 1.0}, {u, v}},
+		{{x, y, z}, {0.0, 1.0}},
+		{{x + width, y, z}, {u, 1.0}},
+		{{x, y + height, z}, {0.0, v}},
+		{{x, y + height, z}, {0.0, v}},
+		{{x + width, y, z}, {u, 1.0}},
+		{{x + width, y + height, z}, {u, v}},
 	};
 
 	memcpy(dest, vertex_list, sizeof(vertex_list));
@@ -92,6 +93,7 @@ static void sceneInit(void)
 	// Get the location of the uniforms
 	uLoc_projection = shaderInstanceGetUniformLocation(program.vertexShader, "projection");
 	uLoc_tint = shaderInstanceGetUniformLocation(program.vertexShader, "tint");
+	uLoc_side = shaderInstanceGetUniformLocation(program.vertexShader, "side");
 
 	// Configure attributes for use with the vertex shader
 	C3D_AttrInfo *attrInfo = C3D_GetAttrInfo();
@@ -100,11 +102,10 @@ static void sceneInit(void)
 	AttrInfo_AddLoader(attrInfo, 1, GPU_FLOAT, 2);
 
 	// Compute the projection matrix
-	 Mtx_OrthoTilt(&projection, 0, 400.0, 240, 0, 0.0, 1.0, true);
-	 
+	Mtx_OrthoTilt(&projection, 0, 400.0, 240, 0, 1000.0, -1000.0, true);
 
 	// Load the texture and bind it to the first texture unit
-	if (!loadTextureFromMem(&texture, NULL, raysQ_t3x, raysQ_t3x_size))
+	if (!loadTextureFromMem(&texture, NULL, raysQ_256_t3x, raysQ_256_t3x_size))
 		svcBreak(USERBREAK_PANIC);
 
 	// Create the VBO (vertex buffer object)
@@ -117,7 +118,7 @@ static void sceneInit(void)
 		float x = randbetween(0, width);
 		float y = randbetween(0, height);
 
-		add_rect(&vbo_data[i * 6], x, y, SPRITE_WIDTH, SPRITE_HEIGHT);
+		add_rect(&vbo_data[i * 6], x, y, randbetween(-20.0, -1), SPRITE_WIDTH, SPRITE_HEIGHT);
 
 		spriteinfo info =  {randbetween(-2, 2), randbetween(-2, 2)};
 		sprites[i] = info;
@@ -128,7 +129,7 @@ static void sceneInit(void)
 	BufInfo_Init(bufInfo);
 	BufInfo_Add(bufInfo, vbo_data, sizeof(vertex), 2, 0x10);
 
-	C3D_TexSetWrap(&texture, GPU_REPEAT, GPU_REPEAT);
+	// C3D_TexSetWrap(&texture, GPU_REPEAT, GPU_REPEAT);
 	C3D_TexSetFilter(&texture, GPU_LINEAR, GPU_NEAREST);
 	C3D_TexBind(0, &texture);
 	// Configure the first fragment shading substage to blend the texture color with
@@ -136,12 +137,10 @@ static void sceneInit(void)
 	// See https://www.opengl.org/sdk/docs/man2/xhtml/glTexEnv.xml for more insight
 	C3D_TexEnv *env = C3D_GetTexEnv(0);
 	C3D_TexEnvInit(env);
-	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_PRIMARY_COLOR, 0);
+	C3D_TexEnvSrc(env, C3D_Both, GPU_TEXTURE0, GPU_CONSTANT, 0);
 	C3D_TexEnvFunc(env, C3D_Both, GPU_MODULATE);
-	// C3D_TexEnvSrc(env, C3D_Alpha, GPU_PRIMARY_COLOR, 0, 0);
-	// C3D_TexEnvFunc(env, C3D_Alpha, GPU_REPLACE);
 
-	C3D_DepthTest(true, GPU_GEQUAL, GPU_WRITE_ALL);
+	C3D_DepthTest(true, GPU_ALWAYS, GPU_WRITE_ALL);
 	C3D_CullFace(GPU_CULL_NONE);
 }
 
@@ -149,12 +148,7 @@ void printvec(C3D_FVec v) {
 	printf("(%f, %f, %f, %f)\n", v.x, v.y, v.z, v.w);
 }
 
-static void sceneRender(void)
-{
-	// Update the uniforms
-	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
-	C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_tint, 1.0f, 1.0f, 1.0f, 1.0f);
-
+static void update(float delta) {
 	for (int i = 0; i < current_sprites; i++) {
 		for (int j = 0; j < 6; j++) {
 			vertex *v = &vbo_data[i * 6 + j];
@@ -169,6 +163,14 @@ static void sceneRender(void)
 			sprites[i].velocity_y *= -1;
 		}
 	}
+}
+
+static void sceneRender(float side)
+{
+	// Update the uniforms
+	C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLoc_projection, &projection);
+	C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_tint, 1.0f, 1.0f, 1.0f, 1.0f);
+	C3D_FVUnifSet(GPU_VERTEX_SHADER, uLoc_side, side, side, side, side);
 
 	// Draw the VBO
 	C3D_DrawArrays(GPU_TRIANGLES, 0, current_sprites * 6);
@@ -187,16 +189,21 @@ static void sceneExit(void)
 	DVLB_Free(vshader_dvlb);
 }
 
+static bool paused = false;
+
 int main()
 {
 	// Initialize graphics
 	gfxInitDefault();
+	gfxSet3D(true);
 	C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
 	consoleInit(GFX_BOTTOM, NULL);
 
 	// Initialize the render target
-	C3D_RenderTarget *target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
-	C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+	C3D_RenderTarget *left_target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTargetSetOutput(left_target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+	C3D_RenderTarget *right_target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+	C3D_RenderTargetSetOutput(right_target, GFX_TOP, GFX_RIGHT, DISPLAY_TRANSFER_FLAGS);
 
 	// Initialize the scene
 	sceneInit();
@@ -206,10 +213,14 @@ int main()
 	{
 		hidScanInput();
 
+		float iod = osGet3DSliderState();
+
 		// Respond to user input
 		u32 kDown = hidKeysDown();
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
+		if (kDown & KEY_SELECT)
+			paused = !paused;
 			
 		u32 kHeld = hidKeysHeld();
 		if ((kHeld & KEY_UP) && current_sprites < MAX_SPRITES)
@@ -222,15 +233,24 @@ int main()
 		if (kHeld & KEY_LEFT)
 			current_sprites = max(current_sprites - 100, 1);
 
+
 		TickCounter start;
 		osTickCounterStart(&start);
 
 		// Render the scene
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+
+		if (!paused)
+			update(1.0);
+
+		printf("\n\n\n\n\n\n\n%f\n", iod);
 		
-		C3D_RenderTargetClear(target, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
-		C3D_FrameDrawOn(target);
-		sceneRender();
+		C3D_RenderTargetClear(left_target, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+		C3D_FrameDrawOn(left_target);
+		sceneRender(iod);
+		C3D_RenderTargetClear(right_target, C3D_CLEAR_ALL, CLEAR_COLOR, 0);
+		C3D_FrameDrawOn(right_target);
+		sceneRender(-iod);
 		C3D_FrameEnd(0);
 		
 		osTickCounterUpdate(&start);
